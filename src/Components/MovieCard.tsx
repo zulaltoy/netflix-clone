85c07bd1fa9dbea61c4c0996607f430c21c6b409
ZulalT <zulal@studaro.com>
2026-03-18 00:17:29 +0100

    Update MovieCard, MovieList components and tsconfig configuration

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
-
+const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 const MovieCard = ({movie}) => {
   return (
     <div>
-      <Card></Card>
+      <Card>
+        <img
+        src={movie.poster_path ? TMDB_IMAGE_BASE_URL + movie?.poster_path : "/placeholder.svg"}
+        alt={movie.title}/>
+      </Card>
       
     </div>
   );
--- src/Components/MovieList.tsx
+++ src/Components/MovieList.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import MovieCard from './MovieCard';
-import type { MovieListProps, Movie } from './types';
+import React from "react";
+import MovieCard from "./MovieCard";
+import type { MovieListProps, Movie } from "./types";
 const MovieList = ({ movies }: MovieListProps) => {
   return (
-    <div>
-        <ul>
-      { movies.map((movie: Movie) => (
-        <li key={movie.id}>
-          <MovieCard movie={movie} />
-        </li>
-      ))}
+    <div className="relative">
+      <ul className="flex overflow-x-scroll overflow-y-visible space-x-4 px-4 md:px-6 py-4 scrollbar-hide relative ">
+        {movies.map((movie: Movie) => (
+          <li key={movie.id}>
+            <MovieCard movie={movie} />
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
